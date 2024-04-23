0dfdad38da349743551e5ea95035e3fd11b82fc1
mariaanko <vaclavik.marian@gmail.com>
2024-04-23 21:12:15 +0200

    run script amended

--- run.sh
+++ run.sh
@@ -4,10 +4,9 @@ echo "starting script"
 
 if [ -f ../fb_token.sh ]
 then
-        source ../fb_token.sh
+  source ../fb_token.sh
+  chmod +x scraper.py
+  nohup python "$HOME"/take-tie-srandicky-eeeej/scraper.py > custom-log.log 2>&1 &
 else
         print "please create a script '../fb_token.sh' and set a FB_TOKEN variable there"
 fi
-
-chmod +x scraper.py
-nohup python "$HOME"/take-tie-srandicky-eeeej/scraper.py > custom-log.log 2>&1 &
