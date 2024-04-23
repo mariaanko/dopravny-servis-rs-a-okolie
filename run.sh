9553d0ed53334546193d3b0a7d4608f18a66be5b
mariaanko <vaclavik.marian@gmail.com>
2024-04-23 20:50:43 +0200

    added run bash script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "starting script"
+
+if [ -f ../fb_token.sh ]
+        source ../fb_token.sh
+else
+        print "please create a script '../fb_token.sh' and set a FB_TOKEN variable there"
+fi
+nohup /home/traffic/take-tie-srandicky-eeeej/scraper.py > custom-output.log
