f2c6ff352470ecbfd9ea37d96e49b1de47f979f4
mariaanko <vaclavik.marian@gmail.com>
2024-04-23 21:46:22 +0200

    15 minutes

--- scraper.py
+++ scraper.py
@@ -58,7 +58,7 @@ def scraper():
                     time_now_seconds = datetime.now()
                     report_seconds = datetime.fromtimestamp(report_millis / 1000)
                     time_difference = time_now_seconds - report_seconds
-                    defined_minutes = timedelta(minutes=30)
+                    defined_minutes = timedelta(minutes=15)
                     print("[" + fmt_report_date + "] : " + alert_type + "-> " + street + ", " + city + " \n")
                     if time_difference <= defined_minutes:
                         if alert_type == 'POLICE' or alert_type == 'ACCIDENT':
@@ -101,7 +101,7 @@ def scraper():
 if __name__ == '__main__':
     print("Process started!")
     scraper()
-    schedule.every(30).minutes.do(scraper)
+    schedule.every(15).minutes.do(scraper)
     print("Scheduled!")
     while True:
         schedule.run_pending()
